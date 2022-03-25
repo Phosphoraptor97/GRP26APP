d72a56410c80768630ccbb7b64b642725f788bd2
zhenyu1311 <101508184+zhenyu1311@users.noreply.github.com>
2022-03-25 09:56:12 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -7,26 +7,11 @@ def driverlogin(request):
     context = {}
     status = ''
 
-    if request.POST:
-        ## Check if customerid is already in the table
-        with connection.cursor() as cursor:
+    
 
-            cursor.execute("SELECT * FROM userInfo WHERE email = %s", [request.POST['email']])
-            customer = cursor.fetchone()
-            ## No customer with same id
-            if customer == None:
-                ##TODO: date validation
-                cursor.execute("INSERT INTO userInfo VALUES (%s, %s, %s, %s)"
-                        , [request.POST['email'], request.POST['firstName'], request.POST['lastName'],
-                           request.POST['username']  ])
-                return redirect('index')    
-            else:
-                status = 'Customer with ID %s already exists' % (request.POST['email'])
-
-
-    context['status'] = status
- 
-    return render(request, "app/add.html", context)    
+            
+       
+    return render(request, "app/driverlogin.html", context)    
     
 def index(request):
     """Shows the main page"""
